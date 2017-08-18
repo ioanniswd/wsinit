3af2d6664a62db645324a5149baa1a0f6ad93a37
ioanniswd <ioanniswd@gmail.com>
2017-08-18 16:30:58 +0300

    Added check for validity of path in repo description. If not  valid, simply clone repo in home folder.
     On branch master
    
     Changes to be committed:
    	modified:   cloneRepo.js
    	new file:   isPathValid.js
    	new file:   test/isPathValid.js

--- cloneRepo.js
+++ cloneRepo.js
@@ -3,8 +3,18 @@
 const exec = require('child_process').exec;
 const home = require('os').homedir();
 
+const isPathValid = require('./isPathValid');
+
 module.exports = function(repo) {
-  var path = `${home}/${repo.local_path}/${repo.name}`;
+
+  var path = `${home}/`;
+
+  if(isPathValid(repo.local_path)) {
+    path += `${repo.local_path}/${repo.name}`;
+
+  } else {
+    path += repo.name;
+  }
 
   return new Promise(function(resolve, reject) {
     exec(`git clone -o upstream ${repo.clone_url} ${path}`, function(err, stdout, stderr) {
